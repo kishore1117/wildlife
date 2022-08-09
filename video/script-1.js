c702f7026795f9a234f65a42dcf8b21ab6a575f4
kishore <kishoreranganathan60@gmail.com>
2022-08-09 18:44:35 +0530

    first commit

---
+++ video/script-1.js
@@ -0,0 +1,34 @@
+
+var div=document.getElementById('#div');
+document.addEventListener("click",function(){
+  console.log("welcome to india");
+ 
+});
+
+window.addEventListener("scroll",function(){
+  let nav=document.querySelector("nav");
+  nav.classList.toggle("sticky",this.window.scrollY > 0)
+
+})
+
+
+    
+
+
+
+    
+
+
+
+
+
+/*function getGreeting(){
+    var hello="welcome you all"; 
+    console.log(hello);
+
+}
+ heading=document.querySelector("#title").value;
+*/
+ 
+     
+
